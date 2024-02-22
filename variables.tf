20ff4de852c2497f24b3fb072b12af55a2d4ecdb
Nimesha-Premaraja <prem.nimesha@prodoscore.com>
2024-02-22 21:10:56 +0530

    chore: add terrafor codes

---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "project_id" {
+  description = "GCP project id"
+  default = ""
+}
+
+variable "pubsub_topic" {
+  default = "scc-alerts"
+  description = "Pubsub topic name"
+}
+
+variable "function_name" {
+  description = "Name of the Cloud Function"
+  default = "scc-alerts"
+}
+
+variable "log_metric" {
+  default = "scc-alerts"
+}
+
+variable "alert_name" {
+  default = "Security Command Center Alerts"
+  description = "Alert Name"
+}
+
+variable "service_account_email" {
+  description = "IAM service account email for the Cloud Function"
+}
+
+variable "notification_channels" {
+  default = ""
+  description = "Notification channels ID of Slack channel"
+} 
+
+variable "function_source" {
+  description = "Cloud Function source code path"
+}
